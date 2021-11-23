064271e9355fc49f014731a69254dee866954a7e
PzzAg6 <57948829+PzzAg6@users.noreply.github.com>
2021-11-23 21:19:46 +0800

    Sad story

--- GD.py
+++ GD.py
@@ -4,3 +4,5 @@ import time
 print('Today is ' + time.strftime("%Y_%m_%d", time.localtime()) + ', it is a good day to take photos!')
 
 print("But my camera is broken!Sosad")
+
+print("And she didn't want to go home with me...")
